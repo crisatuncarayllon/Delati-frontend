c821e071076acbf0a18681f378fa07cbd5aa088e
crisatuncarayllon <58598125+crisatuncarayllon@users.noreply.github.com>
2022-01-08 12:06:18 -0500

    APLICACION FINAL

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/*', (req, res) =>
 
 // Start the app by listening on the default Heroku port
 //app.listen(process.env.PORT || 8080);
-app.listen(3000);//porque el puerto de java es 8080
+app.listen(8080);//porque el puerto de java es 8080
